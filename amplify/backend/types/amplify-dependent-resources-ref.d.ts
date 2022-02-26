881d9eaa2cc7ea67231cfd5ca90e053492ac6542
Michael Liendo <mtliendo@gmail.com>
2022-02-25 20:48:30 -0600

    add amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "coffeecustomer": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
